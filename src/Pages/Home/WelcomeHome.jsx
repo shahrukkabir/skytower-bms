727c4fa4143fd718e9dfc20ebb33a786595890a2
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-09-09 19:56:21 +0600

    completed final design for welcome home page

--- src/Pages/Home/WelcomeHome.jsx
+++ src/Pages/Home/WelcomeHome.jsx
@@ -11,7 +11,7 @@ import img from "../../Image/6.jpg";
 export default function WelcomeHome() {
     const [whatch, setWhatch] = useState(false);
     return (
-        <div className="bg-[#2c241e] relative w-full ">
+        <div className="bg-[#2c241e] mb-20 relative w-full ">
             {/* -----------social icon div---------------- */}
             <div className="w-full h-[80px] z-50 customIndex static sm:absolute -top-[80px] grid sm:flex-row-reverse sm:grid-cols-2 md:grid-cols-3 grid-cols-1">
                 <div className="w-full col-span-1 md:col-span-2 flex justify-center bg-slate-400 sm:bg-transparent sm:justify-end items-center gap-3 sm:gap-10 h-full">
@@ -61,35 +61,35 @@ export default function WelcomeHome() {
                 <div className="hidden md:block md:w-[33.34%] h-full z-0 absolute top-0 right-0">
                     <img src={leftimg} alt="" className="h-full w-full absolute left-0 top-0" />
                 </div>
-                {/* ----------------------contamtDiv------------------ */}
-                <div className="container mx-auto flex flex-col sm:flex-row justify-center items-center gap-10">
+                {/* ----------------------contactDiv------------------ */}
+                <div className="container mx-auto flex flex-col sm:flex-row mr-20 items-center gap-12">
                     <div className="w-full z-10 md:w-1/2">
                         <div className="flex mb-5 flex-col w-[220px] gap-1">
-                            <h3 className="text-center uppercase text-xl text-[#c78960]">
-                                about the building
-                            </h3>
+                            <h3 className="text-center  text-xl text-[#c78960]">
+                                About The Building
+                            </h3>   
                             <img src={design} alt="" />
                         </div>
-                        <h1 className="text-left text-3xl md:text-5xl text-[#c78960]">
-                            The Building You Never Need To Leave
+                        <h1 className="text-left text-xs font-bold text-white md:text-5xl ">
+                            The Building You Never <br /> Need To Leave
                         </h1>
-                        <p className="text-justify text-[#c78960] text-md  my-10">
+                        <p className="text-justify text-[#c78960] mr-24 text-base my-14">
                             Featuring a refined palette of natural materials, the open living
                             spaces provide a warm counterpoint to the soaring ceilings and
                             full-height windows. Custom herringbone floors and hand-selected
                             stone bring a rich tactility to the rooms.
                         </p>
-                        <p className="text-justify text-[#c78960] text-2xl mb-10  ml-10">
-                            The design for LuxTower came from looking at the city`s existing
+                        <p className="text-justify text-white text-lg mb-10 mr-18  ml-24">
+                            The design for LuxTower came from looking at the citys existing
                             buildings and thinking about which ones you might want to live in,
                             not just look at.
-                        </p>
-                        <Link to={"/appartment"} className="cursor-pointer text-center p-1 hover:text-[#2c241e] hover:bg-[#c78960] border-b-2 border-[#c78960] text-[#c78960] m-10">
+                        </p> 
+                        <Link to={"/appartment"} className="cursor-pointer text-center p-1 hover:text-[#2c241e] hover:bg-[#c78960] border-b-2 border-[#c78960] text-[#c78960] m-10 ml-24">
                             SEE RESIDENCES
                         </Link>
                     </div>
-                    <div className="w-full md:w-1/2 p-4 flex justify-center items-center">
-                        <img src={img} alt="" className="shadow-md z-10 border-2 border-white" />
+                    <div className="w-full md:w-1/3 p-4 flex justify-center items-center">
+                        <img src={img} alt="" className="shadow-md z-10" />
                     </div>
                 </div>
             </div>
