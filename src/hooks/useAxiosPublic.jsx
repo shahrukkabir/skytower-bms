da999c3bd0b8d15a8e84dd2efde5c0273956661d
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-11-21 18:56:47 +0600

    final commit with securing server url in env file

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default function useAxiosPublic() {
   return { axiosPublic };
 }
 
-//https://skytower-bms-server.vercel.app/
